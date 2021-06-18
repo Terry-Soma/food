70feee4b2d517842625b63b93224ac2f6208bd26
Terry <jackhack116@yahoo.com>
2021-06-18 08:40:37 +0800

    shivej ugsun query geer hailt hiij uzev

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,22 @@
 import Search from './model/Search';
+// 
+// let search = new Search("pasta");
+// search.doSearch().then(res => console.log(res));
+// 
+// web app tuluv
+// hamgiin amarhan 
+const state = {};
 
-let search = new Search("pasta");
-search.doSearch().then(res => console.log(res));
+const controlSearch = async ()=>{
+  const query="pizza";
+  if(query){
+    state.search = new Search(query);
+    await state.search.doSearch();
+    console.log(state.search.result);
+  } 
+
+}
+document.querySelector(".search").addEventListener("submit",e=>{
+  e.preventDefault();
+  controlSearch();
+});
